53e24814ad6b31973e254c11acc751c25891ad32
Electric_the_dev <112804406+Mario-The-Dev@users.noreply.github.com>
2023-05-06 15:00:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const oberver = new IntersectionObserver((entries) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        }else{
+        }/*else{
             entry.target.classList.remove('show');
-        }
+        }*/
     });
 });
 
